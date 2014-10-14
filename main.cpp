d37afccb4d9ca2469c26430d7faccd290f2b854d
Tobias Becker <becker.tobi@gmail.com>
2014-10-14 16:23:27 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,31 @@
 #include <cppnamespace.h>
 
+/// defines which for naming the namespaces, allows the parameterized buildup of naming schemes 
+#define BASE_NAMESPACE matlib  
+#define MATRIX_NAMESPACE BASE_NAMESPACE, matrix
+#define VECTOR_NAMESPACE BASE_NAMESPACE, vector
+
+NS_BEGIN(MATRIX_NAMESPACE)
+// equivalent to namespace matlib{ namespace matrix{ 
+struct Matrix{
+  auto operator()(int i, int j)->double;
+};
+NS_END(MATRIX_NAMESPACE)
+//equivalent to }}
+
+NS_BEGIN(VECTOR_NAMESPACE)
+struct Vector : public NS(MATRIX_NAMESPACE)::Matrix{ // a vector is a matrix the namespace is accessed via NS(...) macro
+ auto operator()(int i)->double;
+};
+
+NS_END(VECTOR_NAMESPACE)
+
 int main(){
+// using directive to access Vector
+ NS_USE(VECTOR_NAMESPACE);
+
+ Vector v;
+ //global qualification for matrix - also works without using the preprocessor (but makes the code more brittle) 
+ ::matlib::matrix::Matrix m;
 
 }
